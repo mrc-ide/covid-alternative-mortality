6e3b195361ac3448666d89360fbd3fa1c9f3ede4
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2021-11-25 17:33:35 +0000

    script to fit models and estimate sero for Aden

--- analysis/01_aden_model_fit.R
+++ analysis/01_aden_model_fit.R
@@ -6,7 +6,7 @@ library(squire)
 source("R/squire_utils.R")
 source("R/sero_utils.R")
 
-aden_fit_excess_smooth <- function(n_mcmc,rf){
+aden_fit_excess <- function(n_mcmc,rf){
 
   aden_deaths <- readRDS("analysis/data/derived/deaths_time_series/aden_excess_deaths.rds") %>%
     mutate(deaths = as.integer(ifelse(deaths<0,0,deaths)))
@@ -23,11 +23,11 @@ aden_fit_excess_smooth <- function(n_mcmc,rf){
                        hosp_beds = 1133,
                        icu_beds = 17)
 
-  saveRDS(fit,"analysis/data/derived/model_fits/Aden/aden_excess_fit_raw.RDS")
+  saveRDS(fit,"analysis/data/derived/model_fits/Aden/aden_excess_fit_raw.rds")
 
   # then project forward to the end of 2020 assuming Rt stays as estimated at end of fit
   fit_complete <- squire::projections(fit,time_period = 125)
-  saveRDS(fit_complete,"analysis/data/derived/model_fits/Aden/aden_excess_fit_complete.RDS")
+  saveRDS(fit_complete,"analysis/data/derived/model_fits/Aden/aden_excess_fit_complete.rds")
 
 
   # seroprevalence under different assumptions of IgG sero-reversion half-lives
@@ -50,7 +50,7 @@ aden_fit_excess_smooth <- function(n_mcmc,rf){
                                                        igm_conv = 12.3, igm_sens = 1)))
     %>% mutate(scenario="200 days")
   )
-  saveRDS(fit_complete_sero,"analysis/data/derived/seroprevalence/Aden/aden_excess_complete_sero.RDS")
+  saveRDS(fit_complete_sero,"analysis/data/derived/seroprevalence/Aden/aden_excess_complete_sero.rds")
 
 
   return(list(fit = fit, fit_complete = fit_complete, fit_complete_sero = fit_complete_sero))
@@ -60,29 +60,98 @@ aden_fit_excess_smooth <- function(n_mcmc,rf){
 # fit with 100000 mcmc iterations
 # we suggest running on a HPC as is computationally intensive
 
-aden_excess <- aden_fit_excess_smooth(100000,1)
+aden_excess <- aden_fit_excess(100000,1)
 
 # vary IFR in model fit
 
+aden_fit_excess_vary_IFR <- function(n_mcmc,rf,prob_death_scale,IFR){
+
+  aden_deaths <- readRDS("analysis/data/derived/deaths_time_series/aden_excess_deaths.rds") %>%
+    mutate(deaths = as.integer(ifelse(deaths<0,0,deaths)))
+
+  aden_pop <- readRDS("analysis/data/raw/aden_population.RDS")
+
+  fit <- fit_spline_rt_ifr_change(data=aden_deaths,
+                                  country="Yemen",
+                                  population=round(aden_pop$prop*1000000),
+                                  reporting_fraction=rf,
+                                  n_mcmc = n_mcmc,
+                                  replicates = 100,
+                                  rw_duration = 14,
+                                  hosp_beds = 1e10,
+                                  icu_beds = 1e10,
+                                  prob_death_scale = prob_death_scale)
+
+  fit_complete <- squire::projections(fit,time_period = 125)
+  saveRDS(fit_complete,paste0("analysis/data/derived/model_fits/Aden/aden_excess_fit_complete_IFR0",IFR*10,".rds"))
+
+  # log likelihood of seroprevalence under different assumptions of IgG sero-reversion half-life
+
+  ll <- rbind(
+    c("ll"=sero_log_likelihood_aden(seroprev_df_det(fit_complete,
+                                        sero_det = sero_det("iggm", igg_sens = 0.967, igg_conv = 13.3,
+                                                            igg_scale = 109.5,
+                                                            igm_conv = 12.3, igm_sens = 1)),obs_x=549,obs_n=2001),
+      "half_life"=100,"ifr"=IFR),
+    c("ll"=sero_log_likelihood_aden(seroprev_df_det(fit_complete,
+                                               sero_det = sero_det("iggm", igg_sens = 0.967, igg_conv = 13.3,
+                                                                   igg_scale = 132,
+                                                                   igm_conv = 12.3, igm_sens = 1)),obs_x=549,obs_n=2001),
+      "half_life"=120,"ifr"=IFR),
+    c("ll"=sero_log_likelihood_aden(seroprev_df_det(fit_complete,
+                                               sero_det = sero_det("iggm", igg_sens = 0.967, igg_conv = 13.3,
+                                                                   igg_scale = 155,
+                                                                   igm_conv = 12.3, igm_sens = 1)),obs_x=549,obs_n=2001),
+      "half_life"=140,"ifr"=IFR),
+    c("ll"=sero_log_likelihood_aden(seroprev_df_det(fit_complete,
+                                               sero_det = sero_det("iggm", igg_sens = 0.967, igg_conv = 13.3,
+                                                                   igg_scale = 176.5,
+                                                                   igm_conv = 12.3, igm_sens = 1)),obs_x=549,obs_n=2001),
+      "half_life"=160,"ifr"=IFR),
+    c("ll"=sero_log_likelihood_aden(seroprev_df_det(fit_complete,
+                                               sero_det = sero_det("iggm", igg_sens = 0.967, igg_conv = 13.3,
+                                                                   igg_scale = 199,
+                                                                   igm_conv = 12.3, igm_sens = 1)),obs_x=549,obs_n=2001),
+      "half_life"=180,"ifr"=IFR),
+    c("ll"=sero_log_likelihood_aden(seroprev_df_det(fit_complete,
+                                               sero_det = sero_det("iggm", igg_sens = 0.967, igg_conv = 13.3,
+                                                                   igg_scale = 219.67,
+                                                                   igm_conv = 12.3, igm_sens = 1)),obs_x=549,obs_n=2001),
+      "half_life"=200,"ifr"=IFR),
+    c("ll"=sero_log_likelihood_aden(seroprev_df_det(fit_complete,
+                                               sero_det = sero_det("iggm", igg_sens = 0.967, igg_conv = 13.3,
+                                                                   igg_scale = 239,
+                                                                   igm_conv = 12.3, igm_sens = 1)),obs_x=549,obs_n=2001),
+      "half_life"=220,"ifr"=IFR),
+    c("ll"=sero_log_likelihood_aden(seroprev_df_det(fit_complete,
+                                               sero_det = sero_det("iggm", igg_sens = 0.967, igg_conv = 13.3,
+                                                                   igg_scale = 266.67,
+                                                                   igm_conv = 12.3, igm_sens = 1)),obs_x=549,obs_n=2001),
+      "half_life"=240,"ifr"=IFR),
+    c("ll"=sero_log_likelihood_aden(seroprev_df_det(fit_complete,
+                                               sero_det = sero_det("iggm", igg_sens = 0.967, igg_conv = 13.3,
+                                                                   igg_scale = 287.5,
+                                                                   igm_conv = 12.3, igm_sens = 1)),obs_x=549,obs_n=2001),
+      "half_life"=260,"ifr"=IFR),
+    c("ll"=sero_log_likelihood_aden(seroprev_df_det(fit_complete,
+                                               sero_det = sero_det("iggm", igg_sens = 0.967, igg_conv = 13.3,
+                                                                   igg_scale = 310,
+                                                                   igm_conv = 12.3, igm_sens = 1)),obs_x=549,obs_n=2001),
+      "half_life"=280,"ifr"=IFR)
+  )
 
 
 
+  saveRDS(ll,paste0("analysis/data/derived/seroprevalence/Aden/aden_ll_IFR0",IFR*10,".rds"))
+
+  return(list(fit_complete = fit_complete, ll = ll))
 
 
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+aden_excess_IFR02 <- aden_fit_excess_vary_IFR(100000,1,0.2/0.2,0.2)
+aden_excess_IFR03 <- aden_fit_excess_vary_IFR(100000,1,0.3/0.2,0.3)
+aden_excess_IFR04 <- aden_fit_excess_vary_IFR(100000,1,0.4/0.2,0.4)
+aden_excess_IFR05 <- aden_fit_excess_vary_IFR(100000,1,0.6/0.2,0.5)
 
